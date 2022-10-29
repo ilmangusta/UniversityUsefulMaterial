45aad27a90b5edd03cf67c49f9354e2748fc5a7e
Luca Borrello <81256628+ilmangusta@users.noreply.github.com>
2022-10-29 14:09:20 +0200

    Update Main.java

--- Code2/Main.java
+++ Code2/Main.java
@@ -5,37 +5,6 @@ import java.util.*;
 import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 
-
-
-/*
-Scrivere un programma che conta le occorrenze dei caratteri alfabetici 
-(lettere dalla "A" alla "Z") in un insieme di file di testo. 
-Il programma prende in input una serie di percorsi di file testuali 
-e per ciascuno di essi conta le occorrenze dei caratteri, 
-ignorando eventuali caratteri non alfabetici (come per esempio le 
-cifre da 0 a 9). Per ogni file, il conteggio viene effettuato da un 
-apposito task e tutti i task attivati vengono gestiti tramite un pool 
-di thread. I task registrano i loro risultati parziali all'interno di 
-una ConcurrentHashMap. Prima di terminare, il programma stampa su un 
-apposito file di output il numero di occorrenze di ogni carattere. 
-Il file di output contiene una riga per ciascun carattere ed è formattato come segue:
-
-<carattere 1>,<numero occorrenze>
-<carattere 2>,<numero occorrenze>
-...
-<carattere N>,<numero occorrenze>
-
-
-Esempio di file di output:
-
-a,1281
-b,315
-c,261
-d,302
-...
-*/
-
-
 public class Main {
 
     public static void main(String[] args) {
@@ -88,7 +57,6 @@ public class Main {
                 write.write(",");
                 write.write(str);
                 write.write("\n");
-                //System.out.println(entry.getKey() + "," + str); 
             }
             write.close();
         }catch (IOException e) {
