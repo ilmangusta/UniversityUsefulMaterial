4b8a4a919b3a7693759e6af2377badc5a5fdb35e
Luca Borrello <81256628+ilmangusta@users.noreply.github.com>
2022-10-29 14:28:29 +0200

    Update Main.java

--- Code3/Main.java
+++ Code3/Main.java
@@ -4,18 +4,12 @@ import java.util.*;
 import java.util.concurrent.*;
 
 
-public class Main {
-
-
-    
+public class Main {    
     public static void main(String[] args){
         int i=1;
-        //ServerSocket server=null;
         ExecutorService pool=Executors.newCachedThreadPool();
-
         while(true){
             try(ServerSocket server=new ServerSocket(10005)){
-                //System.out.println("Provo ad accettare connesione!");
                 pool.execute(new Server(server.accept(),i));
                 System.out.printf("Connessione accettata...inizio gioco [id:%d]!\n",i);
                 i++;
@@ -23,7 +17,6 @@ public class Main {
             }catch (Exception e) {
                 System.out.println(e.getMessage()+ " errore dal main!");
             }
-
         }
     }
 }
